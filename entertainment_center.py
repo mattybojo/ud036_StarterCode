6b9179c7d45f231ae168f1147ce526f8154740a9
mattybojo <mattbojorquez@gmail.com>
2017-12-03 23:01:18 -0800

    Updated formatting of python files

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,38 +3,45 @@ import media
 
 # create instance of Movie class for each movie - title, storyline, poster link, and youtube trailer link
 
-super_troopers = media.Movie("Super Troopers",
-                             "You dont mess with the law. The law messes with you.",
-                             "https://upload.wikimedia.org/wikipedia/en/1/19/Supertrooper.jpg",
-                             "https://www.youtube.com/watch?v=2-9D2qUHN-E")
+super_troopers = media.Movie(
+    "Super Troopers",
+    "You dont mess with the law. The law messes with you.",
+    "https://upload.wikimedia.org/wikipedia/en/1/19/Supertrooper.jpg",
+    "https://www.youtube.com/watch?v=2-9D2qUHN-E")
 
-shawshank_redemption = media.Movie("Shawshank Redemption",
-                                   "Fear can hold you prisoner. Hope can set you free.",
-                                   "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
-                                   "https://www.youtube.com/watch?v=6hB3S9bIaco")
+shawshank_redemption = media.Movie(
+    "Shawshank Redemption",
+    "Fear can hold you prisoner. Hope can set you free.",
+    "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
+    "https://www.youtube.com/watch?v=6hB3S9bIaco")
 
-forrest_gump = media.Movie("Forrest Gump",
-                           "Life is like a box of chocolates...you never know what you're gonna get.",
-                           "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
-                           "https://www.youtube.com/watch?v=bLvqoHBptjg")
+forrest_gump = media.Movie(
+    "Forrest Gump",
+    "Life is like a box of chocolates...you never know what you're gonna get.",
+    "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
+    "https://www.youtube.com/watch?v=bLvqoHBptjg")
 
-the_matrix = media.Movie("The Matrix",
-                         "In a world of 1s and 0s... are you a zero, or The One?",
-                         "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
-                         "https://www.youtube.com/watch?v=m8e-FF8MsqU")
+the_matrix = media.Movie(
+    "The Matrix",
+    "In a world of 1s and 0s... are you a zero, or The One?",
+    "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+    "https://www.youtube.com/watch?v=m8e-FF8MsqU")
 
-saving_private_ryan = media.Movie("Saving Private Ryan",
-                                  "In the Last Great Invasion of the Last Great War, The Greatest Danger for Eight Men was Saving... One.",
-                                  "https://upload.wikimedia.org/wikipedia/en/a/ac/Saving_Private_Ryan_poster.jpg",
-                                  "https://www.youtube.com/watch?v=zwhP5b4tD6g")
+saving_private_ryan = media.Movie(
+    "Saving Private Ryan",
+    "In the Last Great Invasion of the Last Great War, The Greatest Danger for Eight Men was Saving... One.",
+    "https://upload.wikimedia.org/wikipedia/en/a/ac/Saving_Private_Ryan_poster.jpg",
+    "https://www.youtube.com/watch?v=zwhP5b4tD6g")
 
-the_green_mile = media.Movie("The Green Mile",
-                             "Paul Edgecomb didn't believe in miracles. Until the day he met one.",
-                             "https://upload.wikimedia.org/wikipedia/en/e/e2/The_Green_Mile_%28movie_poster%29.jpg",
-                             "https://www.youtube.com/watch?v=ctRK-4Vt7dA")
+the_green_mile = media.Movie(
+    "The Green Mile",
+    "Paul Edgecomb didn't believe in miracles. Until the day he met one.",
+    "https://upload.wikimedia.org/wikipedia/en/e/e2/The_Green_Mile_%28movie_poster%29.jpg",
+    "https://www.youtube.com/watch?v=ctRK-4Vt7dA")
 
 # array of class Movie that is used in creating the web page
-movies = [super_troopers, shawshank_redemption, forrest_gump, the_matrix, saving_private_ryan, the_green_mile]
+movies = [super_troopers, shawshank_redemption, forrest_gump,
+          the_matrix, saving_private_ryan, the_green_mile]
 
 # create the html file which will contain the movies listed in the movies array
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -3,8 +3,9 @@ import webbrowser
 class Movie():
     """ This class provides a way to store movie related information """
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        """ Initialize the movie class """
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        """ Initialize the Movie class """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
